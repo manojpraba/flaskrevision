8961a99fea12a0a10be40ed93ed900ad42dd6770
manojpraba <mprabakaran792@gmail.com>
2023-07-29 21:37:47 +0530

    newchnages

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ app =Flask(__name__)
 
 @app.route('/')
 def welcome():
-    return "Welcome to the flask"
+    return render_template('calculate.html')
 
-@app.route('/cal',methods=['GET'])
+@app.route('/cal',methods=['POST'])
 def mathoperation():
+    
     operation=request.json["operation"]
     num1=request.json["num1"]
     num2=request.json["num2"]
@@ -22,5 +23,23 @@ def mathoperation():
         result=num1-num2
     return jsonify (result)
 
+@app.route('/calculate',methods=['POST'])
+def mathoperation2():
+    if request.content_type
+    operation=str(request.form["operation"])
+    num1=int(request.form["num1"])
+    num2=int(request.form["num2"])
+
+    if operation=="add":
+        result =num1+num2
+    elif operation =="multiply":
+        result=num1*num2
+    elif operation=="division":
+        result=num1/num2
+    else:
+        result=num1-num2
+    return render_template ('results.html',value=result)
+
 if __name__ == '__main__':
+    #app.run(ssl_context='adhoc')
     app.run(debug=True)
